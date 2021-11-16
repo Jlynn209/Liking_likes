35f547a9877136709d69a803443ad203c2d3b4c6
Jlynn209 <jlynn_209@aol.com>
2021-11-15 23:16:08 -0800

    Made it so you can Create new Messages.

--- script.js
+++ script.js
@@ -3,3 +3,51 @@ function increase(id) {
   var count = parseInt(element.innerText);
   element.innerText = count + 1;
 }
+
+var index = 4;
+
+function createMessage() {
+  var rc = document.getElementById("rc");
+  var newChild = document.createElement("div");
+  newChild.className = "comment-container";
+
+  var likes = 0;
+  var message = prompt("What do you want to tell us?");
+  var name = prompt("What is your name?");
+
+  newChild.innerHTML =
+    '<div class="top">' +
+    '<div class="name">' +
+    "<p>" +
+    name +
+    "</p>" +
+    "</div>" +
+    "<div class='likes'>" +
+    "<p id=" +
+    '"like' +
+    "" +
+    index +
+    '"' +
+    ">" +
+    likes +
+    "like(s)</p>" +
+    "<button onclick=" +
+    "increase" +
+    '("#like' +
+    index +
+    '"' +
+    ")>Like</button>" +
+    "</div>" +
+    "</div>" +
+    '<div class="bottom">' +
+    '<div class="message">' +
+    "<p>" +
+    message +
+    "</p>" +
+    "</div>" +
+    "</div>" +
+    "</div>";
+
+  rc.appendChild(newChild);
+  index++;
+}
